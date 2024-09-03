ebcb2ecb581d63047bc8bd1e7201ad7892cf75f6
Your Name <you@example.com>
2024-09-03 18:12:14 +0530

    new table

--- prisma/migrations/20240830051028_shopvindb/migration.sql
+++ prisma/migrations/20240830051028_shopvindb/migration.sql
@@ -8,3 +8,16 @@ CREATE TABLE "Contact" (
 
     CONSTRAINT "Contact_pkey" PRIMARY KEY ("id")
 );
+CREATE TABLE "Appointment" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "service" TEXT NOT NULL,
+    "doctor" TEXT NOT NULL,
+    "date" TEXT NOT NULL,
+    "time" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Appointment_pkey" PRIMARY KEY ("id")
+);
