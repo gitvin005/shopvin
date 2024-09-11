b548b940dfbbe384375c1d98973aed2190989177
Your Name <you@example.com>
2024-09-11 16:59:00 +0530

    new database table

--- prisma/migrations/20240911112812_appointment/migration.sql
+++ prisma/migrations/20240911112812_appointment/migration.sql
@@ -1,3 +1,6 @@
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('pending', 'complete', 'cancel');
+
 -- CreateTable
 CREATE TABLE "Appointment" (
     "id" SERIAL NOT NULL,
@@ -8,6 +11,7 @@ CREATE TABLE "Appointment" (
     "doctor" TEXT NOT NULL,
     "date" TEXT NOT NULL,
     "time" TEXT NOT NULL,
+    "status" "Status" NOT NULL DEFAULT 'pending',
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "Appointment_pkey" PRIMARY KEY ("id")
